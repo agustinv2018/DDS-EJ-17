26e22fbdfa9d7a3d0220a5a7d433ac3ad9446b8c
agustinv2018 <agustin.v420@gmail.com>
2022-09-16 21:16:24 -0300

    resolucion ejercicio

---
+++ modelo/persona.php
@@ -0,0 +1,50 @@
+<?php
+class Persona {
+    public $Id;
+    public $Nombre;
+    public $Apellido;
+    public $NroDocumento;
+    public $Direccion;
+    public $Email;
+                    
+    public static function BuscarTodas()
+    {
+        $con  = Database::getInstance();
+        $sql = "select * from personas";
+        $queryPersonas = $con->db->prepare($sql);
+        $queryPersonas->execute();
+        $queryPersonas->setFetchMode(PDO::FETCH_CLASS, 'Persona');
+
+        $ListPersonasADevolver = array();
+        
+        foreach ($queryPersonas as $p) {
+            $ListPersonasADevolver[] = $p;
+        }
+
+        return $ListPersonasADevolver;
+    }
+
+    public static function Buscar($id)
+    {
+        $con  = Database::getInstance();
+        $sql = "select * from personas where Id = :p1";
+        $queryPersona = $con->db->prepare($sql);
+        $params = array("p1" => $id);   
+        $queryPersona->execute($params);
+        $queryPersona->setFetchMode(PDO::FETCH_CLASS, 'Persona');                
+        foreach ($queryPersona as $p) {
+            return $p;
+        }
+        
+    }
+
+    public function Agregar()
+    {
+        $con  = Database::getInstance();
+        $sql = "insert into personas (Nombre,Apellido,NroDocumento,Direccion,Email) values (:p1,:p2,:p3,:p4,:p5)";
+        $claseAReemplazar = $con->db->prepare($sql);
+        $params = array("p1" => $this->Nombre, "p2" => $this->Apellido, "p3" => $this->NroDocumento, "p4" => $this->Direccion, "p5" => $this->Email);     
+        $claseAReemplazar->execute($params);
+    }
+
+}
